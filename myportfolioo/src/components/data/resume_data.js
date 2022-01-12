7198e970555fc955146ff430d21b2674a8861daa
rohitsaraswat41 <86039272+rohitsaraswat41@users.noreply.github.com>
2022-01-12 20:23:18 +0530

    portfolio

---
+++ myportfolioo/src/components/data/resume_data.js
@@ -0,0 +1,67 @@
+import react from '../../assets/icons/react.svg'
+import python from '../../assets/icons/python.svg'
+
+const languages = [
+    {
+        icon: react,
+        name: 'Java',
+        level: '70'
+    },
+    {
+        icon: react,
+        name: 'C , C++',
+        level: '70'
+    },
+    {
+        icon: react,
+        name: 'HTML , CSS',
+        level: '90'
+    },
+    {
+        icon: react,
+        name: 'React , Node',
+        level: '60'
+    },
+    {
+        icon: react,
+        name: 'Data Structure & Algo',
+        level: '80'
+    },
+    {
+        icon: react,
+        name: 'Bootstrap , ChakraUI',
+        level: '80'
+    },
+    {
+        icon: react,
+        name: 'Mongodb',
+        level: '80'
+    }
+]
+
+const tools = [
+    {
+        icon: react,
+        name: 'Visual Studio Code',
+        level: '85'
+    },
+    {
+        icon: react,
+        name: 'Hyper',
+        level: '75'
+    },
+    {
+        icon: react,
+        name: 'Atom',
+        level: '90'
+    },
+    {
+        icon: react,
+        name: 'Ecllipse',
+        level: '70'
+    },
+    
+
+]
+
+export { tools, languages }
